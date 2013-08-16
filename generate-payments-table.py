9cb51cd644f43fffcccb78230c595b82d9017c63
Michal Kubenka <mkubenka@gmail.com>
2013-08-17 00:24:44 +0200

    Refactor.

--- generate-payments-table.py
+++ generate-payments-table.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*
 """Payments Table Generator
 
-Usage: generate-payments-table.py <from> <to> --token=<token>
+Usage: 
+  generate-payments-table.py <from> <to> --token=<token>
 
 Options:
   -h --help         Show this screen.
@@ -17,19 +18,19 @@ from fiobank import FioBank
 def main(arguments):
     client = FioBank(token=arguments['--token'])
 
-    gen = client.period(arguments['<from>'], arguments['<to>'])
+    transactions = client.period(arguments['<from>'], arguments['<to>'])
 
-    for g in gen:
+    for transaction in transactions:
         try:
-            g['recipient_message']
+            transactions['recipient_message']
         except KeyError:
-            g['recipient_message'] = ''
+            transaction['recipient_message'] = ''
         try:
-            g['variable_symbol']
+            transaction['variable_symbol']
         except KeyError:
-            g['variable_symbol'] = ''
+            transaction['variable_symbol'] = ''
 
-        print "||%s\t||%s\t||%s\t||%s\t||%s\t||" % (g['date'], g['user_identifiaction'], g['recipient_message'], g['variable_symbol'], g['amount'])
+        print "||%s\t||%s\t||%s\t||%s\t||%s\t||" % (transaction['date'], transaction['user_identifiaction'], transaction['recipient_message'], transaction['variable_symbol'], transaction['amount'])
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Payments Table Generator 0.1')
